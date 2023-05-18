bb96be91f0cc006f44228b00a6ba3960f7f11681
Joseph Collins <joecollins@Josephs-MBP.home>
2023-05-18 14:22:51 +0100

    add color theme with switch in navbar

---
+++ components/ColorThemeSwitch.jsx
@@ -0,0 +1,21 @@
+import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
+
+const ColorThemeSwitch = () => {
+
+    const {toggleColorMode, colorMode} = useColorMode();
+
+    return (
+        <HStack px='20px'>
+            <Switch
+                colorScheme='purple'
+                isChecked={colorMode === 'dark'}
+                onChange={toggleColorMode}
+            />
+            <Text>
+                Dark Mode
+            </Text>
+        </HStack>
+    )
+}
+
+export default ColorThemeSwitch
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { Box, HStack, Image, Show, Text, } from '@chakra-ui/react'
 import logo from '../assets/bitwise_logo.png'
 import { Link, NavLink } from 'react-router-dom';
+import ColorThemeSwitch from './ColorThemeSwitch';
 
 const NavBar = () => {
   return (
@@ -21,7 +22,7 @@ const NavBar = () => {
             <NavLink to="/register/" >Sign Up</NavLink>
           </Box>
           </Show>
-          <Text>Colour Theme</Text>
+          <Text><ColorThemeSwitch /></Text>
           <Show below='lg'>
             Hamberger
           </Show>
