a0594692336dc9b26cdbb2a87b30106264e3c22e
Joseph Collins <joecollins@Josephs-MBP.home>
2023-05-25 12:52:47 +0100

    add placeholder comment input, add skeleton to article on loading

---
+++ src/components/CommentCreate.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Textarea } from "@chakra-ui/react";
+
+const CommentCreate = () => {
+  return (
+    <>
+      <Textarea placeholder="Here is a sample placeholder" />
+    </>
+  );
+};
+
+export default CommentCreate;
--- src/pages/articles/ArticlePage.jsx
+++ src/pages/articles/ArticlePage.jsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 import ArticleCard from "../../components/ArticleCard";
 
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
+import CommentCreate from "../../components/CommentCreate";
+import ArticleCardSkeleton from "../../components/ArticleCardSkeleton";
 
 const ArticlePage = () => {
   const { id } = useParams();
   const [article, setArticle] = useState({ results: [] });
+  const [loaded, setLoaded] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const handleMount = async () => {
@@ -17,21 +21,44 @@ const ArticlePage = () => {
           axiosReq.get(`/articles/${id}`),
         ]);
         setArticle({ results: [article] });
+        setLoaded(true);
         console.log(article);
-      } catch (err) {}
+      } catch (err) {
+        setError(err.message);
+        setLoaded(true);
+      }
     };
 
+    setLoaded(false);
     handleMount();
   }, []);
 
   return (
-    <Flex p={8} flex={1} align={"center"} justify={"center"}>
-      <ArticleCard
-        {...article.results[0]}
-        setArticles={setArticle}
-        articlePage
-      />
-    </Flex>
+    <>
+      {loaded ? (
+        article ? (
+          <Flex p={8} align={"center"} justify={"center"}>
+            <ArticleCard
+              {...article.results[0]}
+              setArticles={setArticle}
+              articlePage
+            />
+          </Flex>
+        ) : (
+          <Text>{"No Article Found"}</Text>
+        )
+      ) : (
+        <Flex p={8} align={"center"} justify={"center"}>
+          <Box w={{ base: "100%", md: "500px" }}>
+            <ArticleCardSkeleton />
+          </Box>
+        </Flex>
+      )}
+
+      <Flex p={8} align={"center"} justify={"center"}>
+        <CommentCreate />
+      </Flex>
+    </>
   );
 };
 
