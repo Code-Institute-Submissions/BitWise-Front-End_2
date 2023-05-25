ecab2c68232855d5b27a39edae04909ccf018c7d
Joseph Collins <joecollins@Josephs-MBP.home>
2023-05-25 17:10:43 +0100

    build create comment form

--- src/components/CommentCreate.jsx
+++ src/components/CommentCreate.jsx
@@ -2,54 +2,71 @@ import React from "react";
 import {
   Box,
   FormControl,
-  FormLabel,
   Textarea,
-  Avatar,
-  HStack,
-  Text,
   Stack,
-  Heading,
+  Button,
   Flex,
 } from "@chakra-ui/react";
 import { useColorModeValue } from "@chakra-ui/react";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { axiosRes } from "../api/axiosDefaults";
+import { RiMailSendLine } from "react-icons/ri";
 
 const CommentCreate = (props) => {
-  const { id, owner, profile_image } = props;
+  const { article, setArticle, setComments } = props;
+  const [body, setBody] = useState("");
 
   const custColor = useColorModeValue("#FAF5FF", "#4A5568");
-  const currentUser = useCurrentUser();
+  const custCommentBg = useColorModeValue("#805AD5", "#2D3748");
+
+  const handleChange = (event) => {
+    setBody(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/comments/", {
+        article,
+        body,
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: [data, ...prevComments.results],
+      }));
+      setArticle((prevArticle) => ({
+        results: [
+          {
+            ...prevArticle.results[0],
+            comments_count: prevArticle.results[0].comments_count + 1,
+          },
+        ],
+      }));
+      setBody("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Box w="100%">
-      <form>
+      <form onSubmit={handleSubmit}>
         <FormControl>
-          <FormLabel>
-            <Heading size="md">Comment:</Heading>
-          </FormLabel>
-          <HStack
-            alignItems="flex-start"
-            borderRadius={"15"}
-            p={2}
-            bg={custColor}
-          >
-            <Link to={`/profiles/`}>
-              <Avatar
-                m={2}
-                name={owner}
-                bg={"purple.500"}
-                src={profile_image}
+          <Stack pb={3} w="100%">
+            <Box borderRadius={5} p={2} bg={custCommentBg} w="100%">
+              <Textarea
+                bg={custColor}
+                placeholder="Enter comment here..."
+                value={body}
+                onChange={handleChange}
               />
-            </Link>
-
-            <Stack pr={3} pb={3} w="100%">
-              <Text>{currentUser?.username}</Text>
-              <Box w="100%">
-                <Textarea bg={custColor} placeholder="Enter comment here..." />
-              </Box>
-            </Stack>
-          </HStack>
+              <Flex mt={2} justifyContent={"end"}>
+                <Button type="submit">
+                  <RiMailSendLine />
+                </Button>
+              </Flex>
+            </Box>
+          </Stack>
         </FormControl>
       </form>
     </Box>
--- src/pages/articles/ArticlePage.jsx
+++ src/pages/articles/ArticlePage.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, Stack, Divider } from "@chakra-ui/react";
 
 import ArticleCard from "../../components/ArticleCard";
 
@@ -6,10 +6,17 @@ import { useParams } from "react-router-dom";
 import CommentCreate from "../../components/CommentCreate";
 import ArticleCardSkeleton from "../../components/ArticleCardSkeleton";
 import useArticle from "../../hooks/UseArticle";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useState } from "react";
 
 const ArticlePage = () => {
   const { id } = useParams();
   const { article, setArticle, error, loaded } = useArticle(`/articles/${id}`);
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const profile_id = currentUser?.profile_id;
+
+  const [comments, setComments] = useState({ results: [] });
 
   return (
     <>
@@ -32,9 +39,35 @@ const ArticlePage = () => {
           </Box>
         </Flex>
       )}
+      <Flex align={"center"} justify={"center"}>
+        <Stack px={10} w="100%">
+          <Flex alignItems={"center"} justifyContent={"center"}>
+            <Heading pb={5} size="lg">
+              Comments
+            </Heading>
+          </Flex>
 
-      <Flex p={8} align={"center"} justify={"center"}>
-        <CommentCreate {...article.results[0]} />
+          <Flex pb={5} alignItems={"center"} justifyContent={"center"}>
+            {comments.results.length ? (
+              <Heading size="md">Join the discussion!</Heading>
+            ) : currentUser ? (
+              <Heading size="md">Be the first to comment!</Heading>
+            ) : (
+              <Heading size="md">Login to comment</Heading>
+            )}
+          </Flex>
+
+          {currentUser && (
+            <CommentCreate
+              profile_id={profile_id}
+              profile_image={profile_image}
+              article={id}
+              setArticle={setArticle}
+              setComments={setComments}
+              currentUser={currentUser}
+            />
+          )}
+        </Stack>
       </Flex>
     </>
   );
