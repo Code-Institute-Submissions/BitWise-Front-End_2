68ccbd320495da007a208dae87593cd42e2c4237
Joseph Collins <joecollins@Josephs-MBP.home>
2023-05-30 13:10:23 +0100

    detail logged in user on left side

--- src/components/LoggedIn.jsx
+++ src/components/LoggedIn.jsx
@@ -1,14 +1,26 @@
 import { useCurrentUser } from "../contexts/CurrentUserContext";
-import { Heading } from "@chakra-ui/react";
+import { HStack, Heading, Link, Avatar } from "@chakra-ui/react";
 
 const LoggedIn = () => {
   const currentUser = useCurrentUser();
   return (
     <>
       {currentUser && (
-        <Heading size="md" pl={5} pt={5}>
-          Logged in as {currentUser.username}
-        </Heading>
+        <HStack justifyContent="flex-start">
+          <Link to={`/profiles/`}>
+            <Avatar
+              size="md"
+              name={currentUser.profile_name}
+              bg={"purple.500"}
+              src={currentUser.profile_image}
+            />
+          </Link>
+          <Heading pl={2} size="md">
+            {currentUser.username.length > 10
+              ? `${currentUser.username.slice(0, 10)}...`
+              : `${currentUser.username}`}
+          </Heading>
+        </HStack>
       )}
     </>
   );
