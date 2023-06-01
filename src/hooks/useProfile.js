ac654afcb0d796cb192f5af9f5f1e912d2edcaf1
Joseph Collins <joecollins@Josephs-MBP.home>
2023-06-01 11:45:28 +0100

    add profile card to profile page with hook for individual profile

---
+++ src/hooks/useProfile.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+import { CanceledError } from "axios";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useProfileData,
+  useSetProfileData,
+} from "../contexts/ProfilesDataContext";
+
+const useProfile = (id) => {
+  const setProfileData = useSetProfileData();
+  const { pageProfile } = useProfileData();
+
+  const [profileArticles, setProfileArticles] = useState({ results: [] });
+
+  const [loaded, setLoaded] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const getProfile = async () => {
+      try {
+        const [{ data: pageProfile }, { data: profileArticles }] =
+          await Promise.all([
+            axiosReq.get(`/profiles/${id}/`, {
+              signal: controller.signal,
+            }),
+            axiosReq.get(`/articles/?owner__profile=${id}`, {
+              signal: controller.signal,
+            }),
+          ]);
+        setProfileData((prevState) => ({
+          ...prevState,
+          pageProfile: { results: [pageProfile] },
+        }));
+        setProfileArticles(profileArticles);
+        setLoaded(true);
+      } catch (err) {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoaded(true);
+      }
+    };
+
+    setLoaded(false);
+    getProfile();
+
+    return () => controller.abort();
+  }, [id, setProfileData]);
+
+  return { pageProfile, setProfileData, profileArticles, error, loaded };
+};
+
+export default useProfile;
--- src/pages/profiles/ProfilePage.jsx
+++ src/pages/profiles/ProfilePage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useProfile from "../../hooks/useProfile";
+import { Text, Box, Card } from "@chakra-ui/react";
+import ProfileCard from "../../components/ProfileCard";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const { id } = useParams();
+  const { pageProfile, setProfileData, profileArticles, error, loaded } =
+    useProfile(id);
+  const profile = pageProfile.results[0];
+
+  return (
+    <Box p={5}>
+      {loaded && <ProfileCard {...profile} setProfileData={setProfileData} />}
+    </Box>
+  );
 };
 
 export default ProfilePage;
