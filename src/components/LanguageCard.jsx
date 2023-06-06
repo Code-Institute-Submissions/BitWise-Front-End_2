3566410748849d922ac768095c5ec50697c2126e
Joseph Collins <joecollins@Josephs-MBP.home>
2023-06-06 12:53:19 +0100

    add language card to profile page

--- src/components/LanguageCard.jsx
+++ src/components/LanguageCard.jsx
@@ -1,15 +1,57 @@
 import React from "react";
-import { Card, CardBody, CardHeader, Heading } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Text,
+  Flex,
+} from "@chakra-ui/react";
 import { Progress } from "@chakra-ui/react";
+import languageOptions from "../constants/languageOptions";
 
 const LanguageCard = (props) => {
-  const { language, confidence, used_since } = props;
+  const { language, confidence, used_since, years_exp } = props;
   return (
-    <Card>
+    <Card mb={5}>
       <CardHeader>
-        <Heading size="sm">{language}</Heading>
+        {languageOptions.map((lan) => {
+          if (lan.value === language) {
+            const IconComponent = lan.icon;
+            return (
+              <Flex mb={2} key={lan.value}>
+                <IconComponent fontSize={20} />
+                <Heading ml={2} size="sm">
+                  {lan.value === "CPlusPlus"
+                    ? "C++"
+                    : lan.value === "CSharp"
+                    ? "C#"
+                    : lan.value}
+                </Heading>
+              </Flex>
+            );
+          }
+          return null;
+        })}
+        {years_exp > 1 ? (
+          <Text>{years_exp} years of experience</Text>
+        ) : (
+          <Text>{years_exp} year of experience</Text>
+        )}
       </CardHeader>
-      <Progress colorScheme="purple" value={confidence} />
+
+      <CardBody>
+        <Heading my={2} size="sm">
+          Confidence ({confidence}/100):
+        </Heading>
+        {confidence > 70 ? (
+          <Progress colorScheme="green" value={confidence} />
+        ) : confidence > 50 ? (
+          <Progress colorScheme="yellow" value={confidence} />
+        ) : (
+          <Progress colorScheme="red" value={confidence} />
+        )}
+      </CardBody>
     </Card>
   );
 };
