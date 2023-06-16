7f44490f7659c45da61aceced1932c5f705bb9e5
Joseph Collins <joecollins@Josephs-MBP.home>
2023-06-16 15:20:41 +0100

    tidy sidbar profiles code

--- src/components/SideGrid.jsx
+++ src/components/SideGrid.jsx
@@ -36,6 +36,7 @@ const SideGrid = () => {
   const iconCustColor = useColorModeValue("white", "black");
   const currentUser = useCurrentUser();
   const navigate = useNavigate();
+  const [showMore, setShowMore] = useState(false); // State to track whether to show more results
 
   const { handleFollow, handleUnFollow } = useFollowProfile();
 
@@ -82,70 +83,81 @@ const SideGrid = () => {
         Popular Profiles:
       </Heading>
 
-      {!loaded && (
+      {loaded ? (
+        sideBarProfiles.results.length ? (
+          <>
+            {sideBarProfiles.results.slice(0, 10).map((profile) => (
+              <Flex
+                pt={3}
+                key={profile.id}
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <Link to={`/profile/${profile.id}`}>
+                  <Box>
+                    {profile.owner.length > 8 ? (
+                      <Text>{profile.owner.slice(0, 12)}...</Text>
+                    ) : (
+                      <Text>{profile.owner}</Text>
+                    )}
+                  </Box>
+                </Link>
+
+                {profile.id === currentUser?.profile_id ? (
+                  <UpdateDeleteButton
+                    icon={<BsThreeDotsVertical />}
+                    target={"Profile"}
+                    handleEdit={() => handleEdit(profile.id)}
+                    handlePasswordUpdate={() =>
+                      handlePasswordUpdate(profile.id)
+                    }
+                    profileUpdate
+                  />
+                ) : profile.following_id ? (
+                  <Button
+                    onClick={() => handleUnFollow(profile)}
+                    bg={custColor}
+                  >
+                    <BiUserMinus color={iconCustColor} fontSize="100%" />
+                  </Button>
+                ) : currentUser ? (
+                  <Button onClick={() => handleFollow(profile)}>
+                    <BiUserPlus fontSize="100%" />
+                  </Button>
+                ) : (
+                  <Popover placement="top">
+                    <PopoverTrigger>
+                      <Button>
+                        <BiUserPlus fontSize="100%" />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent w="220px" marginLeft="-170px">
+                      <PopoverCloseButton />
+                      <PopoverHeader>Sorry...</PopoverHeader>
+                      <PopoverBody>Login to follow</PopoverBody>
+                    </PopoverContent>
+                  </Popover>
+                )}
+              </Flex>
+            ))}
+            {sideBarProfiles.results.length > 10 && (
+              <Box pt={10}>
+                <Link to="/profiles/">
+                  <Heading as="u" color={custColor} size="sm">
+                    View More...
+                  </Heading>
+                </Link>
+              </Box>
+            )}
+          </>
+        ) : (
+          <Text>No Results</Text>
+        )
+      ) : (
         <Flex py={2}>
           <Text pr={2}>Updating...</Text>
           <Spinner />
         </Flex>
-      )}
-
-      {sideBarProfiles.results.slice(0, 10).map((profile) => (
-        <Flex
-          pt={3}
-          key={profile.id}
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          <Link to={`/profile/${profile.id}`}>
-            <Box>
-              {profile.owner.length > 8 ? (
-                <Text>{profile.owner.slice(0, 12)}...</Text>
-              ) : (
-                <Text>{profile.owner}</Text>
-              )}
-            </Box>
-          </Link>
-
-          {profile.id === currentUser?.profile_id ? (
-            <UpdateDeleteButton
-              icon={<BsThreeDotsVertical />}
-              target={"Profile"}
-              handleEdit={() => handleEdit(profile.id)}
-              handlePasswordUpdate={() => handlePasswordUpdate(profile.id)}
-              profileUpdate
-            />
-          ) : profile.following_id ? (
-            <Button onClick={() => handleUnFollow(profile)} bg={custColor}>
-              <BiUserMinus color={iconCustColor} fontSize="100%" />
-            </Button>
-          ) : currentUser ? (
-            <Button onClick={() => handleFollow(profile)}>
-              <BiUserPlus fontSize="100%" />
-            </Button>
-          ) : (
-            <Popover placement="top">
-              <PopoverTrigger>
-                <Button>
-                  <BiUserPlus fontSize="100%" />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent w="220px" marginLeft="-170px">
-                <PopoverCloseButton />
-                <PopoverHeader>Sorry...</PopoverHeader>
-                <PopoverBody>Login to follow</PopoverBody>
-              </PopoverContent>
-            </Popover>
-          )}
-        </Flex>
-      ))}
-      {loaded && (
-        <Box pt={10}>
-          <Link to="/profiles/">
-            <Heading as="u" color={custColor} size="sm">
-              View More...
-            </Heading>
-          </Link>
-        </Box>
       )}
     </>
   );
